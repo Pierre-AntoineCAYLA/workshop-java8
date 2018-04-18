96f40307f6ceed8c97f619f38ba707f3203afaa8
Pierre-Antoine <you@example.com>
2018-04-18 13:47:40 +0200

    exo function-7 ok

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -1,13 +1,10 @@
 package java8.ex06;
 
-
-import java8.data.Data;
 import java8.data.Person;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
 
@@ -19,39 +16,40 @@ import static org.junit.Assert.*;
  */
 public class Function_06_Test {
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
-    // tag::formatAge[]
-    // TODO compléter la méthode
-    // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
-    String formatAge(Supplier<Person> supplier) {
-    	Person suplier=new Person();
-    	suplier.setAge(35);
-        return ("[age="+suplier.getAge()+"]");
-    }
-    // end::formatAge[]
+	// tag::formatAge[]
+	// TODO compléter la méthode
+	// TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>]
+	// (exemple : [age=12])
+	String formatAge(Supplier<Person> supplier) {
+		Person suplier = new Person();
+		suplier.setAge(35);
+		return ("[age=" + suplier.getAge() + "]");
+	}
+	// end::formatAge[]
 
+	@Test
+	public void test_supplier_formatAge() throws Exception {
+		// TODO compléter le test unitaire pour qu'il soit passant
+		String result = formatAge(null);
 
-    @Test
-    public void test_supplier_formatAge() throws Exception {
-        // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+		assertThat(result, is("[age=35]"));
+	}
 
-        assertThat(result, is("[age=35]"));
-    }
+	@Test
+	public void test_supplier_requireNonNull() throws Exception {
+		expectedException.expect(NullPointerException.class);
+		expectedException.expectMessage("require non null object");
 
-    @Test
-    public void test_supplier_requireNonNull() throws Exception {
-        expectedException.expect(NullPointerException.class);
-        expectedException.expectMessage("require non null object");
+		// TODO compléter le test unitaire pour qu'il soit passant
+		Supplier<String> supplier = () -> "require non null object";
 
-        // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier =()->"require non null object";
+		// Avec un paramètre null, cette méthode déclenche un
+		// NullPointerException
+		Objects.requireNonNull(null, supplier);
 
-        // Avec un paramètre null, cette méthode déclenche un NullPointerException
-        Objects.requireNonNull(null, supplier);
-
-    }
+	}
 
 }
